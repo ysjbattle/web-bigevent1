6a716a92ef2ef7742f5415feefec1e6d291dd4d3
闫书剑 <7919157+code-is_power@user.noreply.gitee.com>
2020-10-12 16:45:46 +0800

    完成了主页功能

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,4 +1,25 @@
 $.ajaxPrefilter(function (options) {
-     console.log(options);
+    // console.log(options);
     options.url = `http://ajax.frontend.itheima.net${options.url}`
+
+    // 统一为有权限的接口添加Authorization  请求头
+    if (options.url.includes('/my/')) {
+        options.headers =  {
+            Authorization:localStorage.getItem('token') || ''   
+        }
+    }
+
+    // 全局统一挂载complete回调函数
+    options.complete = function(res) {
+        //  console.log('执行了 complete 回调：')
+        //  console.log(res)
+               console.log(res.responseJSON);
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+          // 1. 强制清空 token
+          localStorage.removeItem('token')
+          // 2. 强制跳转到登录页面
+          location.href = '/login.html'
+        }
+      }
 })
